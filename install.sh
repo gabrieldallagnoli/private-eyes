65561d9e095c2bfff32bf69b77f132a99a24e59a
Gabriel Dallagnoli <187823668+gabrieldallagnoli@users.noreply.github.com>
2025-07-23 20:29:11 -0300

    readd systemd and fix paths

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p $HOME/.assets/private-eyes
-cp -rf main.py $HOME/.assets/private-eyes
+mkdir -p ~/.assets/private-eyes
+cp -rf main.py ~/.assets/private-eyes
+
+mkdir -p ~/.config/systemd/user
+cp -rf private-eyes.service ~/.config/systemd/user/private-eyes.service
+systemctl --user enable --now private-eyes.service
